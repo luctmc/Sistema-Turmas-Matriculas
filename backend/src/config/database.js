c344110092f8cf7e6fa4e91aad37154e2139e201
Pedro Alexandre <pchaves@cijun.sp.gov.br>
2025-10-30 14:28:25 -0300

    fix: Host do banco de dados padrão para execuções locais

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,7 +4,7 @@ export const databaseConfig = {
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'stm',
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT || 5432),
   dialect: 'postgres',
   logging: process.env.DB_LOGGING === 'true' ? console.log : false
